f34abe9ee345320bc77cd0945664383c7e8bbb51
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-12-09 22:55:24 +0300

    notify if 'guessit' not found, capture stdout, no forced key binding

--- open-kinopoisk-page.lua
+++ open-kinopoisk-page.lua
@@ -1,20 +1,22 @@
+
 function callback(success, result, error)
     if result.status == 0 then
-        mp.osd_message("Launched browser", 1)
+        mp.osd_message(result.stdout, 1)
     else
-        mp.osd_message("Unable to find kinopoisk URL", 3)
+        mp.osd_message(result.stdout, 8)
     end
 end
 
-function launch_kinopoisk()
-    mp.osd_message("Finding kinopoisk URL...", 30)
+function mpv_open_kinopoisk_page()
+    mp.osd_message("Finding kinopoisk URL...", 10)
     local script_dir = debug.getinfo(1).source:match("@?(.*/)")
     local table = {}
     table.name = "subprocess"
+    table.playback_only = false
+    table.capture_stdout = true
     table.args = {"python", script_dir.."open-kinopoisk-page.py", mp.get_property("filename")}
     local cmd = mp.command_native_async(table, callback)
 end
 
--- change key binding as desired
-mp.add_forced_key_binding('alt+ctrl+K', 'launch_kinopoisk', launch_kinopoisk)
+mp.add_key_binding('alt+ctrl+K', 'mpv_open_kinopoisk_page', mpv_open_kinopoisk_page)
 
--- open-kinopoisk-page.py
+++ open-kinopoisk-page.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding=utf-8 -*-
 
-from sys import argv, exit, stderr
+from sys import argv, exit
 from webbrowser import open_new_tab
-from guessit import guessit
+
+try:
+    from guessit import guessit
+except ImportError as err:
+    print(f"{err.msg}\nERROR: 'guessit' must be installed in the system!\n")
+    exit(42)
 
 
 def get_kinopoisk_url(filename):
-    result = None
     g = guessit(filename)
     t = g["title"]
     if "year" in g:
         t = "{} {}".format(t, g["year"])
     result = t.replace(' ', '+')
-    search_query = "https://www.kinopoisk.ru/index.php?kp_query="
-    search_query += result
-    return search_query
+    return f"https://www.kinopoisk.ru/index.php?kp_query={result}"
 
 
 if __name__ == "__main__":
     try:
         url = get_kinopoisk_url(argv[1])
+        print("Opening URL in browser")
         open_new_tab(url)
     except ValueError:
-        stderr.write("Failed to find kinopoisk URL")
+        print("Failed to find kinopoisk URL")
         exit(1)
